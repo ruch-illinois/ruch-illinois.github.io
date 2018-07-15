a8a3b7d3f66fa5c3ff19e113f39d332ea198d065
Ricky Uch <rickyu2@illinois.edu>
2018-07-14 23:12:24 -0700

    commit of scatter plot as seperate js script and as standalone file for scatter_plot.html

---
+++ income_net_spend_scatter_plot.js
@@ -0,0 +1,110 @@
+function scatterPlot() {
+
+    var width = 500,
+        height = 500;
+
+    function chart(selection) {
+
+        var data = selection.datum();
+        var div = selection,
+            svg = div.selectAll('svg');
+        svg.attr('width', width).attr('height', height);
+        /*
+         * value accessor - returns the value to encode for a given data object.
+         * scale - maps value to a visual display encoding, such as a pixel position.
+         * map function - maps from data value to display value
+         * axis - sets up axis
+         */
+
+
+        var tooltip = div.attr("class", "tooltip")
+            .style("opacity", 0);
+
+
+    //New version for v4
+
+        var x = d3.scaleLinear()
+            .range([0, width]);
+
+        var y = d3.scaleLinear()
+            .range([height, 0]);
+
+        var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+        var xAxis = d3.axisBottom(x);
+
+        var yAxis = d3.axisLeft(y);
+
+        x.domain(d3.extent(data, function(d) { return d.AVG_NET_PRICE; })).nice();
+        y.domain(d3.extent(data, function(d) { return d.MD_EARN_WNE_P10; })).nice();
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .append("text")
+            .attr("class", "label")
+            .attr("x", width)
+            .attr("y", -6)
+            .style("text-anchor", "end")
+            .text("Average Net Price");
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("class", "label")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Median Earned Income After 10 years")
+
+        svg.selectAll(".dot")
+            .data(data)
+            .enter().append("circle")
+            .attr("class", "dot")
+            .attr("r", 3.5)
+            .attr("cx", function(d) { return x(d.AVG_NET_PRICE); })
+            .attr("cy", function(d) { return y(d.MD_EARN_WNE_P10); })
+            .style("fill", function(d) { return color(d.SCHOOL_TYPE); });
+
+        var legend = svg.selectAll(".legend")
+            .data(color.domain())
+            .enter().append("g")
+            .attr("class", "legend")
+            .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+        legend.append("rect")
+            .attr("x", width - 18)
+            .attr("width", 18)
+            .attr("height", 18)
+            .style("fill", color);
+
+        legend.append("text")
+            .attr("x", width - 24)
+            .attr("y", 9)
+            .attr("dy", ".35em")
+            .style("text-anchor", "end")
+            .text(function(d) { return d; });
+
+    }
+
+    chart.width = function(value) {
+        if (!arguments.length) {
+            return width;
+        }
+        width = value;
+        return chart;
+    };
+
+    chart.height = function(value) {
+        if (!arguments.length) {
+            return height;
+        }
+        height = value;
+        return chart;
+    };
+
+    return chart;
+}
